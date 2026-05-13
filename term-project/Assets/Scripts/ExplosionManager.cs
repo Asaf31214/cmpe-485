2a55377d6808c6df1a5142d509b72c46a7b2a85b
asafk <asaf.kanlipicak@outlook.com>
2026-05-13 06:21:20 +0300

    explosion sound and animation

--- term-project/Assets/Scripts/ExplosionManager.cs
+++ term-project/Assets/Scripts/ExplosionManager.cs
@@ -6,10 +6,22 @@ public static class ExplosionManager
     public const float Force = 500f;
     public const float Damage = 100f;
 
+    private static AudioClip _explosionClip;
+
     public static void Create(Vector3 position)
     {
         CreateVisual(position);
+        PlaySound(position);
         ApplyExplosion(position);
+    }
+
+    private static void PlaySound(Vector3 position)
+    {
+        if (_explosionClip == null)
+            _explosionClip = Resources.Load<AudioClip>("explosion");
+
+        if (_explosionClip != null)
+            AudioSource.PlayClipAtPoint(_explosionClip, position);
     }
 
     private static void CreateVisual(Vector3 position)
@@ -17,14 +29,14 @@ public static class ExplosionManager
         var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         go.name = "Explosion";
         go.transform.position = position;
-        go.transform.localScale = Vector3.one * Radius * 2;
+        go.transform.localScale = Vector3.one * Radius * 0.5f;
 
         var mat = go.GetComponent<Renderer>().material;
         mat.shader = Shader.Find("Particles/Standard Unlit");
-        mat.color = new Color(1f, 0.5f, 0f);
+        mat.color = new Color(1f, 0.5f, 0f, 1f);
 
         Object.Destroy(go.GetComponent<Collider>());
-        Object.Destroy(go, 0.3f);
+        go.AddComponent<ExplosionVisual>();
     }
 
     private static void ApplyExplosion(Vector3 position)
